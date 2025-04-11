f738522d9b0528083fae183850bdacd37d4573f5
Doug <doughkruse@gmail.com>
2025-04-10 21:31:09 -0700

    wrap app in ThemeProvider and CssBaseline (light mode default)

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { getTheme } from "./theme";
 
-//eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+// Theme is set to 'light' by default to preserve current appearance
+// Change to 'dark' to test appearance under dark mode
+const theme = getTheme("dark");
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/jedi-on-tauntauns/" element={<App />}></Route>
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/jedi-on-tauntauns/" element={<App />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
